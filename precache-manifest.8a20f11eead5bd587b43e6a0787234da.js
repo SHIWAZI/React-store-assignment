d787cc5f80cf44078fae7488cbf26d3f52f04aef
SHIWAZI <er.shiwazidey@hotmail.com>
2020-09-03 17:25:41 +0530

    Updates

---
+++ precache-manifest.8a20f11eead5bd587b43e6a0787234da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d9cf3e6f8b56ef44f44143dfb0ac81",
+    "url": "/React-store-assignment/index.html"
+  },
+  {
+    "revision": "74ac170155e0b38a8271",
+    "url": "/React-store-assignment/static/css/main.ba6b10da.chunk.css"
+  },
+  {
+    "revision": "ea28b8d147aaa3c2f587",
+    "url": "/React-store-assignment/static/js/2.7b1bc5b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-store-assignment/static/js/2.7b1bc5b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74ac170155e0b38a8271",
+    "url": "/React-store-assignment/static/js/main.bbac0b85.chunk.js"
+  },
+  {
+    "revision": "ac15a93a0179dcb976dc",
+    "url": "/React-store-assignment/static/js/runtime-main.099aec0d.js"
+  }
+]);
